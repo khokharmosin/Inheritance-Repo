2f93582e5960fcc64907110afdf3bd0b0af7c84e
khokharmosin <khokharmosin7806@gmail.com>
2022-04-13 10:59:17 +0530

    MODIFIED: Student Info Program using Hierachical Inheritance

--- src/com/infogalaxy/Result.java
+++ src/com/infogalaxy/Result.java
@@ -1,31 +1,16 @@
 package com.infogalaxy;
 
-public class Result extends Internal {
-    int total;
-    int perc;
-
-    void calculate() {
-        total = im1 + im2;
-        perc = total / 2;
-    }
-
-    void getResult() {
-        System.out.println("ID : " + id);
-        System.out.println("Name : " + name);
-        System.out.println("Im1 Marks : " + im1);
-        System.out.println("Im2 Marks : " + im2);
-        System.out.println("Total Marks : " + total);
-        System.out.println("Percentage : " + perc);
-    }
-}
-
 class Main {
     public static void main(String[] args) {
-        com.infogalaxy.Result result = new com.infogalaxy.Result();
-        result.setStudent();
-        result.setMarks();
-        result.calculate();
-        result.getResult();
+        Internal internal = new Internal();
+        External external = new External();
+        internal.setStudent();
+        internal.setMarks();
+        internal.getinternal();
+        external.setStudent();
+        external.setexternal();
+        external.getexternal();
+
     }
 }
 
--- src/com/infogalaxy/Student.java
+++ src/com/infogalaxy/Student.java
@@ -19,9 +19,35 @@ class Internal extends Student {
     int im2;
 
     void setMarks() {
-        System.out.println("Enter The Im1 Marks : ");
+        System.out.println("Enter The Im1 Mark : ");
         im1 = scanner.nextInt();
-        System.out.println("Enter The Im2 Marks : ");
+        System.out.println("Enter The Im2 Mark : ");
         im2 = scanner.nextInt();
+    }
+
+    void getinternal() {
+        System.out.println("ID : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("IM1 Mark : " + im1);
+        System.out.println("IM2 Mark : " + im2);
+    }
+}
+
+class External extends Internal {
+    int em1;
+    int em2;
+
+    void setexternal() {
+        System.out.println("Enter The EM1 Mark : ");
+        em1 = scanner.nextInt();
+        System.out.println("Enter The EM2 Mark : ");
+        em2 = scanner.nextInt();
+    }
+
+    void getexternal() {
+        System.out.println("ID : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("EM1 Mark : " + em1);
+        System.out.println("EM2 mark : " + em2);
     }
 }
